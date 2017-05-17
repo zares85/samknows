faea7674e9ef0e8964ab1c58a9668a7a52d3bb00
Carlos Jurado <carlos.jurado@kineo.com>
2017-05-17 17:44:36 +0100

    Implementing BasicAggregation and passing their tests

--- src/Samknows/Aggregation/BasicAggregation.php
+++ src/Samknows/Aggregation/BasicAggregation.php
@@ -13,16 +13,37 @@ namespace Samknows\Aggregation;
 class BasicAggregation implements Aggregation {
 
     /**
+     * @var int
+     */
+    protected $unitId;
+
+    /**
+     * @var $int
+     */
+    protected $hour;
+
+    /**
+     * BasicAggregation constructor.
+     *
+     * @param int $unitId
+     * @param $hour
+     */
+    public function __construct($unitId, $hour) {
+        $this->unitId = $unitId;
+        $this->hour = $hour;
+    }
+
+    /**
      * @return int
      */
     public function unitId() {
-        // TODO: Implement unitId() method.
+        return $this->unitId;
     }
 
     /**
      * @return int between 0 and 23
      */
     public function hour() {
-        // TODO: Implement hour() method.
+        return $this->hour;
     }
 }
