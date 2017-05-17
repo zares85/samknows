84b2c157ab1e76dc1db7b2ddb3afeb162aa44096
Carlos Jurado <carlos.jurado@kineo.com>
2017-05-17 18:00:30 +0100

    Implementing BasicResult and pass their test

--- src/Samknows/Result/BasicResult.php
+++ src/Samknows/Result/BasicResult.php
@@ -15,37 +15,79 @@ use Samknows\Aggregation\Aggregation;
 class BasicResult implements Result {
 
     /**
+     * @var Aggregation
+     */
+    protected $aggregation;
+
+    /**
+     * @var float
+     */
+    protected $mean;
+
+    /**
+     * @var float
+     */
+    protected $minimum;
+
+    /**
+     * @var float
+     */
+    protected $maximum;
+
+    /**
+     * @var float
+     */
+    protected $median;
+
+    /**
+     * BasicResult constructor.
+     *
+     * @param Aggregation $aggregation
+     * @param float $mean
+     * @param float $minimum
+     * @param float $maximum
+     * @param float $median
+     */
+    public function __construct(Aggregation $aggregation, $mean, $minimum, $maximum, $median) {
+        $this->aggregation = $aggregation;
+        $this->mean = $mean;
+        $this->minimum = $minimum;
+        $this->maximum = $maximum;
+        $this->median = $median;
+    }
+
+    /**
      * @return Aggregation
      */
     public function aggregation() {
-        // TODO: Implement aggregation() method.
+        return $this->aggregation;
     }
 
     /**
      * @return float
      */
     public function mean() {
-        // TODO: Implement mean() method.
+        return $this->mean;
     }
 
     /**
      * @return float
      */
     public function minimum() {
-        // TODO: Implement minimum() method.
+        return $this->minimum;
     }
 
     /**
      * @return float
      */
     public function maximum() {
-        // TODO: Implement maximum() method.
+        return $this->maximum;
     }
 
     /**
      * @return float
      */
     public function median() {
-        // TODO: Implement median() method.
+        return $this->median;
     }
 }
