ebe4eab04516fd6d849a97c678a23563dc8f0be6
Carlos Jurado <carlos.jurado@kineo.com>
2017-05-18 00:09:10 +0100

    Add Aggregate Command

---
+++ src/Samknows/Command/AggregateCommand.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * AggregateCommand.php
+ *
+ * @package
+ * @author    Carlos Jurado <carlos.jurado@kineo.com>
+ * @copyright 2017 City and Guilds Kineo
+ * @license   http://www.kineo.com
+ */
+
+namespace Samknows\Command;
+
+use Samknows\Aggregation\BasicAggregation;
+use Samknows\Repository\MetricRepository;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AggregateCommand extends ContainerCommand {
+
+    protected function configure() {
+        $this
+            ->setName('aggregate')
+            ->setDescription('Install tables into database.')
+            ->addArgument('unit', InputArgument::REQUIRED, 'Unit id')
+            ->addArgument('metric', InputArgument::REQUIRED, 'download, upload, latency, packet_loss')
+            ->addArgument('hour', InputArgument::OPTIONAL, '1 to 23')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $unit = $input->getArgument('unit');
+        $metric = $input->getArgument('metric');
+        $hour = $input->getArgument('hour');
+
+        /** @var MetricRepository $repository */
+        $repository = $this->container['MetricRepository'];
+        $aggregation = new BasicAggregation($unit, $hour, $metric);
+        $result = $repository->aggregate($aggregation);
+
+        $output->writeln("Mean {$result->mean()}");
+        $output->writeln("Minimum {$result->minimum()}");
+        $output->writeln("Maximum {$result->maximum()}");
+        $output->writeln("Median {$result->median()}");
+        $output->writeln("Sample size {$result->count()}");
+    }
+
+}
--- src/Samknows/Repository/IlluminateMetricRepository.php
+++ src/Samknows/Repository/IlluminateMetricRepository.php
@@ -11,6 +11,7 @@
 namespace Samknows\Repository;
 
 use Illuminate\Database\ConnectionInterface;
+use Illuminate\Database\Query\Builder;
 use Samknows\Aggregation\Aggregation;
 use Samknows\Metric\Metric;
 use Samknows\Result\BasicResult;
@@ -85,7 +86,45 @@ class IlluminateMetricRepository implements MetricRepository {
      * @return Result
      */
     public function aggregate(Aggregation $aggregation) {
-        // TODO: Implement aggregate() method.
-        return null;
+        $builder = $this->db->table($aggregation->metric())->where('unit_id', '=', $aggregation->unitId());
+
+        if ($hour = $aggregation->hour()) {
+            $builder = $builder->whereRaw('HOUR(`ts`) = ?', [$aggregation->hour()]);
+        }
+
+        return new BasicResult(
+            $aggregation,
+            $builder->avg('value'),
+            $builder->min('value'),
+            $builder->max('value'),
+            $this->mean($builder),
+            $builder->count()
+        );
+    }
+
+    /**
+     * Calculate the mean.
+     *
+     * count the number of rows in the table with
+     * SELECT COUNT(*) FROM {table name}
+     * if the retrieved number of rows is odd, get the value directly with
+     * SELECT {column name} FROM {table name} ORDER BY {column name} LIMIT {(total number of rows in table-1)/2},1
+     * if the number of rows is even, get the two relevant values from the table with
+     * SELECT {column name} FROM {table name} ORDER BY {column name} LIMIT {(total number of rows in table/2)-1},2
+     * add them afterwards and divide the results by 2
+     *
+     * @param Builder $builder
+     * @return float|int
+     */
+    protected function mean(Builder $builder) {
+        $count = $builder->count();
+
+        if ($count % 2) {
+            $limit = ($count - 1) / 2;
+            return $builder->limit("{$limit}, 1")->value('value');
+        } else {
+            $limit = ($count / 2) - 1;
+            return $builder->limit("{$limit}, 2")->value('value') / 2;
+        }
     }
 }
